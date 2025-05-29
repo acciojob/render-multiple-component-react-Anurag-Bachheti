09cc529780a661e80f9f46ea3d736bbe207ef358
HERCULES0000 <anuragbachheti1999@gmail.com>
2025-05-29 15:50:38 +0530

    State managed dynamically

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,32 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 const App = () => {
+
+  let projects = [
+    {
+      name: "Ai music composer",
+      description: "A tool to play music compositions"
+    },{
+      name: "Eco-Friendly Smart Home",
+      description: "A home automation system focusing on sustainability."
+    },
+    {
+      name: "Virtual Travel Guide",
+      description: "An immersive app offering virtual tours of destinations."
+    }
+  ];
+
+
   return (
-    <div id="main"></div>
+    <div id="main" className="ns-wrapper">
+      {projects.map((project, index)=> (
+        <div key={index}>
+          <h1 className={"project-name"}> {project.name} </h1>
+          <h6 className={"project-description"}> {project.description} </h6>
+        </div>
+      ))}
+    </div>
   )
 }
-
 
 export default App;
